429a0edc682eb62eb74831c4fbf9561e63fe0380
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-22 23:28:24 +0200

    Add files via upload

---
+++ 04.Sequence2k+1/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = int.Parse(Console.ReadLine());
+int firstNum = 1;
+while(firstNum <= num)
+{
+    Console.WriteLine(firstNum);
+    firstNum = firstNum * 2 + 1;
+}
---
+++ 06. NumberinRange/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+while (num < 1 || num > 100)
+{
+    num = int.Parse(Console.ReadLine());
+
+}
+Console.WriteLine(num);
