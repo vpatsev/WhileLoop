c19a6db67d0b10c72a5e57bf03b913463ca0e87a
Ventsislav Patsev <164578745+vpatsev@users.noreply.github.com>
2025-01-22 23:28:52 +0200

    Add files via upload

---
+++ 07. NumberProcessor/Program.cs
@@ -0,0 +1,23 @@
+﻿int num = int.Parse(Console.ReadLine());
+string command = Console.ReadLine();
+
+while (num > 0)
+{
+    if (command == "Inc")
+    {
+        num++;
+        command = Console.ReadLine();
+
+    }
+    if (command == "Dec")
+    {
+        num--;
+        command = Console.ReadLine();
+
+    }
+    if (command == "End")
+    {
+        Console.WriteLine(num);
+        break;
+    }
+}
